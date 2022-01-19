3109fdb97fa9a529ca3d8947fc3657facd46c267
shaista78 <shaistasulthana78@gmail.com>
2022-01-19 09:29:06 +0530

    FizzBuzz

---
+++ FizzBuzz.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("hello world");
+    }
+    
+}
