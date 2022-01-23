507ab95a688ca6bf884b757e7d3bb9ead16e558b
shaista78 <shaistasulthana78@gmail.com>
2022-01-23 21:47:09 +0530

    linked list basics

---
+++ BubbleSort/BubblrSort.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class BubblrSort {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++) arr[i]=s.nextInt();
+        
+
+    }
+}
